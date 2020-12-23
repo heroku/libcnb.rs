b7372671380c0c4d1168450bf5546deabd5d2233
Terence Lee <hone02@gmail.com>
2020-12-23 13:11:49 -0600

    add BuildpackApi type

--- src/data/buildpack.rs
+++ src/data/buildpack.rs
@@ -2,13 +2,13 @@ use crate::Error;
 use lazy_static::lazy_static;
 use regex::Regex;
 use semver::Version;
-use serde::Deserialize;
-use std::str::FromStr;
+use serde::{de, Deserialize};
+use std::{fmt, str::FromStr};
 
 #[derive(Deserialize, Debug)]
 pub struct BuildpackToml {
     // MUST be in form <major>.<minor> or <major>, where <major> is equivalent to <major>.0.
-    pub api: String,
+    pub api: BuildpackApi,
     pub buildpack: Buildpack,
     pub stacks: Vec<Stack>,
     pub metadata: toml::value::Table,
@@ -28,6 +28,76 @@ pub struct Buildpack {
 pub struct Stack {
     pub id: StackId,
     pub mixins: Vec<String>,
+}
+
+#[derive(Debug)]
+pub struct BuildpackApi {
+    pub major: u32,
+    pub minor: u32,
+}
+
+impl FromStr for BuildpackApi {
+    type Err = Error;
+
+    fn from_str(value: &str) -> Result<Self, Self::Err> {
+        lazy_static! {
+            static ref RE: Regex = Regex::new(r"^(?P<major>\d+)\.(?P<minor>\d*)$").unwrap();
+        }
+
+        let string = String::from(value);
+        if let Some(captures) = RE.captures(value) {
+            if let Some(major) = captures.name("major") {
+                // these should never panic since we check with the regex unless it's greater than
+                // `std::u32::MAX`
+                let major = match major.as_str().parse::<u32>() {
+                    Ok(parsed) => parsed,
+                    Err(_) => return Err(Error::InvalidBuildpackApi(string)),
+                };
+                // If no minor version is specified default to 0.
+                let minor = match captures
+                    .name("minor")
+                    .map(|s| s.as_str())
+                    .unwrap_or("0")
+                    .parse::<u32>()
+                {
+                    Ok(parsed) => parsed,
+                    Err(_) => return Err(Error::InvalidBuildpackApi(string)),
+                };
+
+                return Ok(BuildpackApi { major, minor });
+            }
+        }
+
+        Err(Error::InvalidBuildpackApi(string))
+    }
+}
+
+impl<'de> de::Deserialize<'de> for BuildpackApi {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: de::Deserializer<'de>,
+    {
+        struct BuildpackApiVisitor;
+
+        impl<'de> de::Visitor<'de> for BuildpackApiVisitor {
+            type Value = BuildpackApi;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str(
+                    "a BuildpackApi as a string which MUST be in form <major>.<minor> or <major>",
+                )
+            }
+
+            fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+            where
+                E: de::Error,
+            {
+                BuildpackApi::from_str(v).map_err(de::Error::custom)
+            }
+        }
+
+        deserializer.deserialize_str(BuildpackApiVisitor)
+    }
 }
 
 /// buildpack.toml Buildpack Id. This is a newtype wrapper around a String. It MUST only contain numbers, letters, and the characters ., /, and -. It also cannot be `config` or `app`. Use [`std::str::FromStr`] to create a new instance of this struct.
