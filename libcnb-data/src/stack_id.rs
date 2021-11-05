b990576d6de4fde67c375be59fae52bfea87ca6e
Manuel Fuchs <manuel.fuchs@salesforce.com>
2021-11-05 14:14:52 +0100

    Improve interoperability of StackId

--- libcnb-data/src/stack_id.rs
+++ libcnb-data/src/stack_id.rs
@@ -1,6 +1,8 @@
 use lazy_static::lazy_static;
 use regex::Regex;
 use serde::Deserialize;
+use std::borrow::Borrow;
+use std::ops::Deref;
 use std::str::FromStr;
 
 /// Stack Id. This is a newtype wrapper around a String.
@@ -20,18 +22,45 @@ use std::str::FromStr;
 /// let invalid = StackId::from_str("!nvalid");
 /// assert!(invalid.is_err());
 ///
-/// let valid = StackId::from_str("*");
-/// assert!(valid.is_ok());
+/// // StackId implements various traits to improve interoperability. We can, for example, call
+/// // methods of String on StackId or pass it to functions that expect a String/str:
+/// let stack_id = "heroku-20".parse::<StackId>().unwrap();
+///
+/// assert_eq!("HEROKU-20", stack_id.to_uppercase());
+///
+/// fn length(s: &str) -> usize {
+///     s.len()
+/// }
+///
+/// assert_eq!(9, length(&stack_id));
 /// ```
 #[derive(Deserialize, Debug)]
 pub struct StackId(String);
 
-#[derive(thiserror::Error, Debug)]
-pub enum StackIdError {
-    #[error(
-    "Found `{0}` but value MUST only contain numbers, letters, and the characters `.`, `/`, and `-`. or only `*`"
-    )]
-    InvalidStackId(String),
+impl StackId {
+    pub fn as_str(&self) -> &str {
+        &self.0
+    }
+}
+
+impl Borrow<String> for StackId {
+    fn borrow(&self) -> &String {
+        &self.0
+    }
+}
+
+impl Deref for StackId {
+    type Target = String;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl AsRef<String> for StackId {
+    fn as_ref(&self) -> &String {
+        &self.0
+    }
 }
 
 impl FromStr for StackId {
@@ -51,14 +80,10 @@ impl FromStr for StackId {
     }
 }
 
-impl From<StackId> for String {
-    fn from(stack_id: StackId) -> Self {
-        stack_id.0
-    }
-}
-
-impl StackId {
-    pub fn as_str(&self) -> &str {
-        &self.0
-    }
+#[derive(thiserror::Error, Debug)]
+pub enum StackIdError {
+    #[error(
+    "Found `{0}` but value MUST only contain numbers, letters, and the characters `.`, `/`, and `-`. or only `*`"
+    )]
+    InvalidStackId(String),
 }
