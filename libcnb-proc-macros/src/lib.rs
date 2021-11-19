f11f02a4e6c7936fe1537fa9072d9b5457b5a3a5
Manuel Fuchs <manuel.fuchs@salesforce.com>
2021-11-19 18:38:40 +0100

    Enable clippy::pedantic and unused_crate_dependencies warnings

--- libcnb-proc-macros/src/lib.rs
+++ libcnb-proc-macros/src/lib.rs
@@ -1,3 +1,9 @@
+// Enable rustc and Clippy lints that are disabled by default.
+// https://doc.rust-lang.org/rustc/lints/listing/allowed-by-default.html#unused-crate-dependencies
+#![warn(unused_crate_dependencies)]
+// https://rust-lang.github.io/rust-clippy/stable/index.html
+#![warn(clippy::pedantic)]
+
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::parse::{Parse, ParseStream};
