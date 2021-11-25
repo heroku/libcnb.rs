884c5bd0581549b57013714a1f31f4caaefe1746
Ed Morley <501702+edmorley@users.noreply.github.com>
2021-11-25 13:21:44 +0000

    Deserialize BuildpackApi directly instead of via a shadow struct (#203)
    - Removes `BuildpackApiUnchecked`, since it is unnecessary as it only
      wrapped a `String`.
    - Moves the parsing from the `FromStr` to the `TryFrom` implementation,
      and removes the now unused `FromStr` implementation.
    - Switches tests from manual `toml::from_str()` calls to using helpers from
     `serde_test`: https://serde.rs/unit-testing.html
    
    GUS-W-10229170.

--- libcnb-data/src/buildpack/api.rs
+++ libcnb-data/src/buildpack/api.rs
@@ -1,39 +1,26 @@
 use std::convert::TryFrom;
+use std::fmt;
 use std::fmt::{Display, Formatter};
-use std::{fmt, str::FromStr};
 
 use fancy_regex::Regex;
 use lazy_static::lazy_static;
 use serde::Deserialize;
 
-// Used as a "shadow" struct to store
-// potentially invalid `BuildpackApi` data when deserializing
-// <https://dev.to/equalma/validate-fields-and-types-in-serde-with-tryfrom-c2n>
-#[derive(Deserialize)]
-struct BuildpackApiUnchecked(String);
-
-impl TryFrom<BuildpackApiUnchecked> for BuildpackApi {
-    type Error = BuildpackApiError;
-
-    fn try_from(value: BuildpackApiUnchecked) -> Result<Self, Self::Error> {
-        Self::from_str(value.0.as_str())
-    }
-}
-
 /// The Buildpack API version.
 ///
 /// This MUST be in form `<major>.<minor>` or `<major>`, where `<major>` is equivalent to `<major>.0`.
 #[derive(Deserialize, Debug, Eq, PartialEq)]
-#[serde(try_from = "BuildpackApiUnchecked")]
+#[serde(try_from = "&str")]
 pub struct BuildpackApi {
     pub major: u32,
     pub minor: u32,
 }
 
-impl FromStr for BuildpackApi {
-    type Err = BuildpackApiError;
+impl TryFrom<&str> for BuildpackApi {
+    type Error = BuildpackApiError;
 
-    fn from_str(value: &str) -> Result<Self, Self::Err> {
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        // We're not using the `semver` crate, since it only supports non-range versions of form `X.Y.Z`.
         lazy_static! {
             static ref RE: Regex = Regex::new(r"^(?P<major>\d+)(\.(?P<minor>\d+))?$").unwrap();
         }
@@ -45,20 +32,20 @@ impl FromStr for BuildpackApi {
                 let major = major
                     .as_str()
                     .parse::<u32>()
-                    .map_err(|_| Self::Err::InvalidBuildpackApi(String::from(value)))?;
+                    .map_err(|_| Self::Error::InvalidBuildpackApi(String::from(value)))?;
 
                 // If no minor version is specified default to 0.
                 let minor = captures
                     .name("minor")
                     .map_or("0", |s| s.as_str())
                     .parse::<u32>()
-                    .map_err(|_| Self::Err::InvalidBuildpackApi(String::from(value)))?;
+                    .map_err(|_| Self::Error::InvalidBuildpackApi(String::from(value)))?;
 
                 return Ok(Self { major, minor });
             }
         }
 
-        Err(Self::Err::InvalidBuildpackApi(String::from(value)))
+        Err(Self::Error::InvalidBuildpackApi(String::from(value)))
     }
 }
 
@@ -76,67 +63,63 @@ pub enum BuildpackApiError {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use serde_test::{assert_de_tokens, assert_de_tokens_error, Token};
 
-    // Containing struct required due to: https://github.com/alexcrichton/toml-rs/issues/253
-    #[derive(Deserialize, Debug, Eq, PartialEq)]
-    struct TestBuildpackToml {
-        api: BuildpackApi,
-    }
+    use super::*;
 
     #[test]
     fn deserialize_valid_api_versions() {
-        assert_eq!(
-            toml::from_str::<TestBuildpackToml>("api = '1.3'"),
-            Ok(TestBuildpackToml {
-                api: BuildpackApi { major: 1, minor: 3 }
-            }),
+        assert_de_tokens(
+            &BuildpackApi { major: 1, minor: 3 },
+            &[Token::BorrowedStr("1.3")],
         );
-        assert_eq!(
-            toml::from_str::<TestBuildpackToml>("api = '0.0'"),
-            Ok(TestBuildpackToml {
-                api: BuildpackApi { major: 0, minor: 0 }
-            }),
+        assert_de_tokens(
+            &BuildpackApi { major: 0, minor: 0 },
+            &[Token::BorrowedStr("0.0")],
         );
-        assert_eq!(
-            toml::from_str::<TestBuildpackToml>("api = '2020.10'"),
-            Ok(TestBuildpackToml {
-                api: BuildpackApi {
-                    major: 2020,
-                    minor: 10
-                }
-            }),
+        assert_de_tokens(
+            &BuildpackApi {
+                major: 2020,
+                minor: 10,
+            },
+            &[Token::BorrowedStr("2020.10")],
         );
-        assert_eq!(
-            toml::from_str::<TestBuildpackToml>("api = '2'"),
-            Ok(TestBuildpackToml {
-                api: BuildpackApi { major: 2, minor: 0 }
-            }),
+        assert_de_tokens(
+            &BuildpackApi { major: 2, minor: 0 },
+            &[Token::BorrowedStr("2")],
         );
     }
 
     #[test]
     fn reject_invalid_api_versions() {
-        let err = toml::from_str::<TestBuildpackToml>("api = '1.2.3'").unwrap_err();
-        assert!(err.to_string().contains("Found `1.2.3` but value MUST"));
-
-        let err = toml::from_str::<TestBuildpackToml>("api = '1.2-dev'").unwrap_err();
-        assert!(err.to_string().contains("Found `1.2-dev` but value MUST"));
-
-        let err = toml::from_str::<TestBuildpackToml>("api = '-1'").unwrap_err();
-        assert!(err.to_string().contains("Found `-1` but value MUST"));
-
-        let err = toml::from_str::<TestBuildpackToml>("api = '.1'").unwrap_err();
-        assert!(err.to_string().contains("Found `.1` but value MUST"));
-
-        let err = toml::from_str::<TestBuildpackToml>("api = '1.'").unwrap_err();
-        assert!(err.to_string().contains("Found `1.` but value MUST"));
-
-        let err = toml::from_str::<TestBuildpackToml>("api = '1..2'").unwrap_err();
-        assert!(err.to_string().contains("Found `1..2` but value MUST"));
-
-        let err = toml::from_str::<TestBuildpackToml>("api = ''").unwrap_err();
-        assert!(err.to_string().contains("Found `` but value MUST"));
+        assert_de_tokens_error::<BuildpackApi>(
+            &[Token::BorrowedStr("1.2.3")],
+            "Found `1.2.3` but value MUST be in the form `<major>.<minor>` or `<major>` and only contain numbers.",
+        );
+        assert_de_tokens_error::<BuildpackApi>(
+            &[Token::BorrowedStr("1.2-dev")],
+             "Found `1.2-dev` but value MUST be in the form `<major>.<minor>` or `<major>` and only contain numbers.",
+        );
+        assert_de_tokens_error::<BuildpackApi>(
+            &[Token::BorrowedStr("-1")],
+             "Found `-1` but value MUST be in the form `<major>.<minor>` or `<major>` and only contain numbers.",
+        );
+        assert_de_tokens_error::<BuildpackApi>(
+            &[Token::BorrowedStr(".1")],
+             "Found `.1` but value MUST be in the form `<major>.<minor>` or `<major>` and only contain numbers.",
+        );
+        assert_de_tokens_error::<BuildpackApi>(
+            &[Token::BorrowedStr("1.")],
+             "Found `1.` but value MUST be in the form `<major>.<minor>` or `<major>` and only contain numbers.",
+        );
+        assert_de_tokens_error::<BuildpackApi>(
+            &[Token::BorrowedStr("1..2")],
+             "Found `1..2` but value MUST be in the form `<major>.<minor>` or `<major>` and only contain numbers.",
+        );
+        assert_de_tokens_error::<BuildpackApi>(
+            &[Token::BorrowedStr("")],
+             "Found `` but value MUST be in the form `<major>.<minor>` or `<major>` and only contain numbers.",
+        );
     }
 
     #[test]
