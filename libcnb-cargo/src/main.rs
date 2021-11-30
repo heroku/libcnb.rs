e863d5074f0b9b8fa9ab13160c20d9ab45b352d9
Manuel Fuchs <manuel.fuchs@salesforce.com>
2021-11-30 13:35:02 +0100

    Improve Clap config

--- libcnb-cargo/src/main.rs
+++ libcnb-cargo/src/main.rs
@@ -182,12 +182,17 @@ fn handle_libcnb_package(matches: &ArgMatches) {
 fn setup_cli_parsing<'a, 'b>() -> clap::App<'a, 'b> {
     App::new(env!("CARGO_PKG_NAME"))
         .bin_name("cargo")
+        .version(env!("CARGO_PKG_VERSION"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .setting(AppSettings::GlobalVersion)
         .setting(AppSettings::SubcommandRequiredElseHelp)
         .subcommand(
             SubCommand::with_name("libcnb")
+                .about("Allows working with buildpacks written with libcnb.rs")
                 .setting(AppSettings::SubcommandRequiredElseHelp)
                 .subcommand(
                     SubCommand::with_name("package")
+                        .about("Packages a libcnb.rs Cargo project as a Cloud Native Buildpack")
                         .arg(
                             Arg::with_name("release")
                                 .long("release")
