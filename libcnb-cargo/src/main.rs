66d27b57364ecbd3143f96ef1782e98d229c3ed9
Manuel Fuchs <manuel.fuchs@salesforce.com>
2021-11-30 15:42:44 +0100

    Move setup_cli_parsing into cli.rs

--- libcnb-cargo/src/main.rs
+++ libcnb-cargo/src/main.rs
@@ -3,8 +3,9 @@
 // This lint is too noisy and enforces a style that reduces readability in many cases.
 #![allow(clippy::module_name_repetitions)]
 
+mod cli;
 use cargo_metadata::MetadataCommand;
-use clap::{App, AppSettings, Arg, ArgMatches, SubCommand};
+use clap::ArgMatches;
 use libcnb_cargo::cross_compile::cross_compile_help;
 use libcnb_cargo::{
     assemble_buildpack_tarball, build_buildpack_binary, default_buildpack_tarball_filename,
@@ -18,7 +19,7 @@ use std::path::PathBuf;
 fn main() {
     setup_logging();
 
-    match setup_cli_parsing().get_matches().subcommand() {
+    match cli::setup_cli_parsing().get_matches().subcommand() {
         ("libcnb", Some(matches)) => match matches.subcommand() {
             ("package", Some(matches)) => handle_libcnb_package(matches),
             // This should never be reached since clap will catch unknown subcommands for us
@@ -173,42 +174,6 @@ fn handle_libcnb_package(matches: &ArgMatches) {
 
     info!("Packaging successfully finished!");
     info!("Hint: To test your buildpack locally with pack, run: pack build my-image --buildpack {} --path /path/to/application", relative_output_path.to_string_lossy());
-}
-
-fn setup_cli_parsing<'a, 'b>() -> clap::App<'a, 'b> {
-    App::new(env!("CARGO_PKG_NAME"))
-        .bin_name("cargo")
-        .version(env!("CARGO_PKG_VERSION"))
-        .about(env!("CARGO_PKG_DESCRIPTION"))
-        .setting(AppSettings::GlobalVersion)
-        .setting(AppSettings::SubcommandRequiredElseHelp)
-        .subcommand(
-            SubCommand::with_name("libcnb")
-                .about("Allows working with buildpacks written with libcnb.rs")
-                .setting(AppSettings::SubcommandRequiredElseHelp)
-                .subcommand(
-                    SubCommand::with_name("package")
-                        .about("Packages a libcnb.rs Cargo project as a Cloud Native Buildpack")
-                        .arg(
-                            Arg::with_name("release")
-                                .long("release")
-                                .help("Build in release mode, with optimizations"),
-                        )
-                        .arg(
-                            Arg::with_name("target")
-                                .long("target")
-                                .default_value("x86_64-unknown-linux-musl")
-                                .help("Build for the target triple"),
-                        )
-                        .arg(
-                            Arg::with_name("output-path")
-                                .long("output")
-                                .short("o")
-                                .help("Write buildpack tarball to this path instead of Cargo's target directory")
-                                .takes_value(true),
-                        ),
-                ),
-        )
 }
 
 fn setup_logging() {
