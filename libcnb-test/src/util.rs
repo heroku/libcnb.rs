ff5e09c6a259b5fa2c194b7aaebc40f9ee082b93
Manuel Fuchs <manuel.fuchs@salesforce.com>
2022-01-25 14:40:26 +0100

    Add random_docker_identifier docs

--- libcnb-test/src/util.rs
+++ libcnb-test/src/util.rs
@@ -1,6 +1,11 @@
 use rand::{thread_rng, Rng};
 
-pub fn random_docker_identifier() -> String {
+/// Generate a random Docker identifier.
+///
+/// It is suitable to be used as an image tag or container name.
+///
+/// See: [Docker Image Specification](https://github.com/moby/moby/blob/master/image/spec/v1.1.md)
+pub(crate) fn random_docker_identifier() -> String {
     format!(
         "libcnbtest_{}",
         thread_rng()
