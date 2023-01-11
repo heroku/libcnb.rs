dc1556ee10778127dfb3ea00f931d76068a6ef0b
Richard Schneeman <rschneeman@salesforce.com>
2023-01-11 15:30:19 -0600

    Introduce Changelog parsing and for prepare script
    Parse the CHANGELOG.md to see what has been changed, added, or fixed. Use this information to determine an appropriate SerVer level. Use that level to update the Cargo.toml versions and then finally update the CHANGELOG.md with the local date.

--- scripts/prepare/src/main.rs
+++ scripts/prepare/src/main.rs
@@ -1,3 +1,7 @@
+use comrak::arena_tree::Node;
+use comrak::nodes::NodeValue::{Heading, List, Text};
+use comrak::nodes::{Ast, NodeHeading};
+use std::cell::RefCell;
 use std::{
     env,
     path::{Path, PathBuf},
@@ -6,24 +10,168 @@ use std::{
 
 fn main() {
     let root = libcnb_root().expect("Could not determine root of libcnb.rs");
-    println!("Found project root at {}", root.display());
-
+    let changelog = root.join("CHANGELOG.md");
     let cargo = root.join("Cargo.toml");
+
+    println!("Found project root at {}", root.display());
+    println!("\n## Detecting semver from CHANGELOG.md\n");
+
+    let changelog_contents =
+        std::fs::read_to_string(&changelog).expect("Could not read in CHANGELOG.md");
+    let semver_level = semver_from_changelog(&changelog_contents)
+        .expect("Could not determine SemVer from CHANGELOG.md");
+
+    println!("semver change is: {:?}", semver_level);
     println!("\n## Updating Cargo.toml {}\n", cargo.display());
 
-    let level = SemVerLevel::Patch;
     let mut doc = toml_doc_from(&cargo).expect("Could not parse Cargo.toml");
 
     let old_version =
         workspace_version_from_doc(&doc).expect("Could not parse version from Cargo.toml");
-    println!("Last release was version '{}'", old_version);
+    println!("Last release was version: '{}'", old_version);
 
-    let new_version = apply_semver(&old_version, level).expect("Could not parse version as semver");
+    let new_version =
+        apply_semver(&old_version, semver_level).expect("Could not parse version as semver");
     println!("Updated version {}", new_version);
 
     update_versions_in_doc(&mut doc, &new_version);
 
-    write_doc_to_file(doc, &cargo).expect("Could not update Cargo.toml")
+    write_doc_to_file(doc, &cargo).expect("Could not update Cargo.toml");
+
+    println!("\n## Updating CHANGELOG.md {}\n", changelog.display());
+
+    let unreleased_re = regex::Regex::new(r"## \[Unreleased\]").expect("Regex is invalid");
+    if unreleased_re.is_match(&changelog_contents) {
+        let changelog_contents = unreleased_re
+            .replacen(
+                &changelog_contents,
+                1,
+                format!(
+                    "## [Unreleased]\n\n{}",
+                    version_ymd_md_string(&new_version, chrono::Local::now())
+                ),
+            )
+            .to_string();
+
+        std::fs::write(changelog, changelog_contents).expect("Could not update CHANGELOG.md");
+    } else {
+        panic!("Could not find `## [Unreleased]` in changelog")
+    }
+}
+
+fn is_header(node: &Node<RefCell<Ast>>, desired_level: u32) -> bool {
+    if let Heading(NodeHeading { level, .. }) = &(*node.data.borrow()).value {
+        if level == &desired_level {
+            return true;
+        }
+    }
+    false
+}
+
+fn child_contains(node: &Node<RefCell<Ast>>, desired_contains: &str) -> bool {
+    if let Some(node) = node.first_child() {
+        if let Text(contents) = &(*node.data.borrow()).value {
+            if std::str::from_utf8(&contents)
+                .unwrap()
+                .contains(desired_contains)
+            {
+                return true;
+            }
+        }
+    }
+    false
+}
+
+fn sibling_is_list(node: &Node<RefCell<Ast>>) -> bool {
+    if let Some(node) = node.next_sibling() {
+        if let List(_) = &(*node.data.borrow()).value {
+            return true;
+        }
+    }
+    false
+}
+
+fn semver_from_changelog(changelog: &str) -> Result<SemVerLevel, BePreparedError> {
+    let arena = comrak::Arena::new();
+    let root = comrak::parse_document(&arena, changelog, &comrak::ComrakOptions::default());
+    let mut nodes = root.children().peekable();
+
+    // Find the Unreleased section, it's a header node that has a single child, a text node
+    let mut unreleased = None;
+    while let Some(node) = nodes.next() {
+        if is_header(node, 2) && child_contains(node, "[Unreleased]") {
+            unreleased = Some(node);
+            break;
+        }
+    }
+
+    if unreleased.is_none() {
+        return Err(BePreparedError::Editorial(String::from(
+            "Missing the `## Unreleased` header from CHANGELOG.md",
+        )));
+    }
+
+    // Check search sub headers to find fixed, added, and changed header nodes
+    let mut fixed = None;
+    let mut changed = None;
+    let mut added = None;
+    while let Some(node) = nodes.next() {
+        if is_header(node, 2) {
+            // Too far
+            break;
+        }
+
+        if is_header(node, 3) {
+            if child_contains(node, "Fixed") {
+                fixed = Some(node)
+            }
+            if child_contains(node, "Added") {
+                added = Some(node)
+            }
+            if child_contains(node, "Changed") {
+                changed = Some(node)
+            }
+        }
+    }
+
+    if let Some(node) = changed {
+        if sibling_is_list(node) {
+            return Ok(SemVerLevel::Major);
+        }
+    } else {
+        println!("Warning: Changelog.md is missing a `### Fixed` section");
+    }
+
+    if let Some(node) = added {
+        if sibling_is_list(node) {
+            return Ok(SemVerLevel::Minor);
+        }
+    } else {
+        println!("Warning: Changelog.md is missing a `### Added` section");
+    }
+
+    if let Some(node) = fixed {
+        if sibling_is_list(node) {
+            return Ok(SemVerLevel::Patch);
+        }
+    } else {
+        println!("Warning: Changelog.md is missing a `### Added` section");
+    }
+
+    Err(BePreparedError::Editorial(String::from(
+        "No changes found in the CHANGELOG.md cannot continue",
+    )))
+}
+
+fn version_ymd_md_string<D: chrono::Datelike>(version: &str, now: D) -> String {
+    format!(
+        "## [{}] {}-{:0width$}-{:0width$}",
+        version,
+        now.year(),
+        now.month(),
+        now.day(),
+        width = 2,
+    )
 }
 
 fn write_doc_to_file(doc: toml_edit::Document, cargo_toml: &Path) -> Result<(), BePreparedError> {
@@ -120,4 +268,87 @@ enum BePreparedError {
     TomlError(toml_edit::TomlError),
     SemverError(semver::Error),
     Editorial(String),
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use chrono::prelude::*;
+
+    #[test]
+    fn semver_errors_without_unreleased() {
+        let changelog = r"
+
+## Ooops unr3l3453d accidentally deleted
+            ";
+        assert!(semver_from_changelog(changelog).is_err());
+    }
+
+    #[test]
+    fn semver_from_changelog_finds_major_minor_patch_okay() {
+        let changelog = r"
+## [Unreleased]
+
+### Fixed
+
+- The world
+
+### Changed
+
+- The game
+
+### Added
+
+- Good measure
+            ";
+        assert_eq!(
+            SemVerLevel::Major,
+            semver_from_changelog(changelog).unwrap()
+        );
+
+        let changelog = r"
+## [Unreleased]
+
+### Fixed
+
+- The world
+
+### Changed
+
+### Added
+
+- Good measure
+";
+        assert_eq!(
+            SemVerLevel::Minor,
+            semver_from_changelog(changelog).unwrap()
+        );
+
+        let changelog = r"
+## [Unreleased]
+
+### Fixed
+
+- The world
+
+### Changed
+
+### Added
+
+";
+        assert_eq!(
+            SemVerLevel::Patch,
+            semver_from_changelog(changelog).unwrap()
+        );
+    }
+
+    #[test]
+    fn test_ymd_version() {
+        let date = NaiveDate::from_ymd_opt(2000, 1, 12).unwrap();
+        assert_eq!(
+            "## [1.1.1] 2000-01-12",
+            version_ymd_md_string("1.1.1", date)
+        );
+    }
 }
