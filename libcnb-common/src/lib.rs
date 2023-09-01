5de5a66e2419a466f0c8bef64c761fde7125fad6
Manuel Fuchs <manuel.fuchs@salesforce.com>
2023-09-01 14:03:30 +0200

    Include README.md in lib.rs (#667)

--- libcnb-common/src/lib.rs
+++ libcnb-common/src/lib.rs
@@ -1,3 +1,4 @@
+#![doc = include_str!("../README.md")]
 #![warn(clippy::pedantic)]
 #![warn(unused_crate_dependencies)]
 // This lint is too noisy and enforces a style that reduces readability in many cases.
