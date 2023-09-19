51d49693388e05d8f28301710fa67a3151b71f53
Colin Casey <casey.colin@gmail.com>
2023-09-19 09:37:03 -0300

    Update libcnb-test/tests/integration_test.rs
    Co-authored-by: Ed Morley <501702+edmorley@users.noreply.github.com>

--- libcnb-test/tests/integration_test.rs
+++ libcnb-test/tests/integration_test.rs
@@ -546,7 +546,7 @@ fn address_for_port_when_container_crashed() {
 #[ignore = "integration test"]
 fn basic_build_with_libcnb_reference_to_single_buildpack() {
     TestRunner::default().build(
-        BuildConfig::new("heroku/builder:22", "test-fixtures/procfile").buildpacks(vec![
+        BuildConfig::new("heroku/builder:22", "test-fixtures/empty").buildpacks(vec![
             BuildpackReference::WorkspaceBuildpack(buildpack_id!("libcnb-test/a")),
         ]),
         |context| {
