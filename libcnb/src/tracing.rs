aae2e57561ad32c0bdf9263f0cd5384d96c79407
Josh W Lewis <josh.lewis@salesforce.com>
2023-11-08 12:43:27 -0600

    Add a tracing test

--- libcnb/src/tracing.rs
+++ libcnb/src/tracing.rs
@@ -16,10 +16,10 @@ pub(crate) struct BuildpackTrace {
 }
 
 pub(crate) fn start_trace(buildpack: &Buildpack, phase_name: &'static str) -> BuildpackTrace {
-    let bp_slug = buildpack.id.replace(['/', '.'], "_");
+    let trace_name = format!("{}-{phase_name}", buildpack.id.replace(['/', '.'], "_"));
     let tracing_file_path = Path::new("/tmp")
         .join("cnb-telemetry")
-        .join(format!("{bp_slug}-{phase_name}.jsonl"));
+        .join(format!("{trace_name}.jsonl"));
 
     // Ensure tracing file path parent exists by creating it.
     if let Some(parent_dir) = tracing_file_path.parent() {
@@ -57,7 +57,7 @@ pub(crate) fn start_trace(buildpack: &Buildpack, phase_name: &'static str) -> Bu
         None,
     );
 
-    let mut span = tracer.start(phase_name);
+    let mut span = tracer.start(trace_name);
     span.set_attributes(vec![
         KeyValue::new("buildpack_id", buildpack.id.to_string().clone()),
         KeyValue::new("buildpack_name", buildpack.name.clone().unwrap_or_default()),
@@ -82,7 +82,60 @@ impl BuildpackTrace {
 
 impl Drop for BuildpackTrace {
     fn drop(&mut self) {
+        self.span.end();
         self.provider.force_flush();
         global::shutdown_tracer_provider();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::start_trace;
+    use libcnb_data::buildpack::{Buildpack, BuildpackVersion};
+    use std::{
+        collections::HashSet,
+        fs,
+        io::{Error, ErrorKind},
+    };
+
+    #[test]
+    fn test_tracing() {
+        let buildpack = Buildpack {
+            id: "company.com/foo"
+                .parse()
+                .expect("Valid BuildpackId should parse"),
+            version: BuildpackVersion::new(0, 0, 0),
+            name: Some("Foo buildpack for company.com".to_string()),
+            homepage: None,
+            clear_env: false,
+            description: None,
+            keywords: vec![],
+            licenses: vec![],
+            sbom_formats: HashSet::new(),
+        };
+        let phase = "bar";
+        let event = "baz-event";
+        let error_message = "its broken";
+        let telemetry_path = "/tmp/cnb-telemetry/company_com_foo-bar.jsonl";
+        _ = fs::remove_file(telemetry_path);
+
+        {
+            let mut trace = start_trace(&buildpack, &phase);
+            trace.add_event(event);
+            trace.set_error(&Error::new(ErrorKind::Other, error_message));
+        }
+        let tracing_contents = fs::read_to_string(telemetry_path)
+            .expect("Expected telemetry file to exist, but couldn't read it");
+
+        println!("tracing_contents: {tracing_contents}");
+        assert!(tracing_contents.contains(phase));
+        assert!(tracing_contents.contains(event));
+        assert!(tracing_contents.contains(error_message));
+        assert!(tracing_contents.contains(buildpack.id.as_str()));
+        assert!(tracing_contents.contains(&buildpack.version.to_string()));
+        assert!(
+            tracing_contents.contains(&buildpack.name.expect("Expected buildpack.name to exist"))
+        );
+        assert!(tracing_contents.contains("something"));
     }
 }
