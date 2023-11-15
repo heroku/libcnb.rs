adb88f9a906a45e17fe8defd5bb7eb3ee51040e7
Josh W Lewis <josh.lewis@salesforce.com>
2023-11-15 11:37:36 -0600

    Add some documentation to tracing.rs

--- libcnb/src/tracing.rs
+++ libcnb/src/tracing.rs
@@ -8,15 +8,28 @@ use opentelemetry_sdk::{
     trace::{Config, Span, TracerProvider},
     Resource,
 };
-use std::path::Path;
+use std::{io::BufWriter, path::Path};
 
+// This is the diretory in which `BuildpackTrace` stores Open Telemetry File
+// Exports. Services which intend to export the tracing data from libcnb.rs
+// (such as [cnb-otel-collector](https://github.com/heroku/cnb-otel-collector))
+// should look for `.jsonl` file exports in this directory. This path was chosen
+// to prevent conflicts with the CNB spec and /tmp is commonly available and
+// writable on base images.
+#[cfg(target_family = "unix")]
 const TELEMETRY_EXPORT_ROOT: &str = "/tmp/cnb-telemetry";
 
+/// `BuildpackTrace` represents an Open Telemetry tracer provider and single span.
+/// It's designed to support tracing a CNB build or detect phase as a singular
+/// span.
 pub(crate) struct BuildpackTrace {
     provider: TracerProvider,
     span: Span,
 }
 
+/// `start_trace` starts an Open Telemetry trace and span that exports to an
+/// Open Telemetry file export. The resulting trace provider and span are
+/// enriched with data from the buildpack and the rust environment.
 pub(crate) fn start_trace(buildpack: &Buildpack, phase_name: &'static str) -> BuildpackTrace {
     let trace_name = format!("{}-{phase_name}", buildpack.id.replace(['/', '.'], "_"));
     let tracing_file_path = Path::new(TELEMETRY_EXPORT_ROOT).join(format!("{trace_name}.jsonl"));
@@ -30,28 +43,42 @@ pub(crate) fn start_trace(buildpack: &Buildpack, phase_name: &'static str) -> Bu
         .append(true)
         .open(&tracing_file_path)
     {
+        // Write tracing data to a file, which may be read by other
+        // services. Wrap with a BufWriter to prevent serde from sending each
+        // JSON token to IO, and instead send entire JSON objects to IO.
         Ok(file) => opentelemetry_stdout::SpanExporter::builder()
-            .with_writer(file)
+            .with_writer(BufWriter::new(file))
             .build(),
+        // Failed tracing shouldn't fail a build, and any logging here would
+        // likely confuse the user, so send telemetry to /dev/null on errors.
         Err(_) => opentelemetry_stdout::SpanExporter::builder()
             .with_writer(std::io::sink())
             .build(),
     };
 
-    let lib_name = option_env!("CARGO_PKG_NAME").unwrap_or("libcnb");
-    let lib_version = option_env!("CARGO_PKG_VERSION");
-
     let provider = TracerProvider::builder()
         .with_simple_exporter(exporter)
         .with_config(Config::default().with_resource(Resource::new(vec![
-            KeyValue::new("service.name", lib_name),
-            KeyValue::new("service.version", lib_version.unwrap_or_default()),
+            // Associate the tracer provider with service attributes. The buildpack
+            // name/version seems to map well to the suggestion
+            // [here](https://opentelemetry.io/docs/specs/semconv/resource/#service).
+            KeyValue::new("service.name", buildpack.id.to_string()),
+            KeyValue::new("service.version", buildpack.version.to_string()),
         ])))
         .build();
 
+    // Set the global tracer provider so that buildpacks may use it.
     global::set_tracer_provider(provider.clone());
 
-    let tracer = provider.versioned_tracer(lib_name, lib_version, None as Option<&str>, None);
+    // Get a tracer identified by the instrumentation scope/library. The crate
+    // name/version seems to map well to the suggestion
+    // [here](https://opentelemetry.io/docs/specs/otel/trace/api/#get-a-tracer).
+    let tracer = provider.versioned_tracer(
+        option_env!("CARGO_PKG_NAME").unwrap_or("libcnb.rs"),
+        option_env!("CARGO_PKG_VERSION"),
+        None as Option<&str>,
+        None,
+    );
 
     let mut span = tracer.start(trace_name);
     span.set_attributes(vec![
@@ -67,10 +94,13 @@ pub(crate) fn start_trace(buildpack: &Buildpack, phase_name: &'static str) -> Bu
 }
 
 impl BuildpackTrace {
+    /// `set_error` sets the status for the underlying span to error, and
+    /// also records an exception on the span.
     pub(crate) fn set_error(&mut self, err: &dyn std::error::Error) {
-        self.span.set_status(Status::error(err.to_string()));
+        self.span.set_status(Status::error(format!("{err:?}")));
         self.span.record_error(err);
     }
+    /// `add_event` adds a named event to the underlying span.
     pub(crate) fn add_event(&mut self, name: &'static str) {
         self.span.add_event(name, vec![]);
     }
