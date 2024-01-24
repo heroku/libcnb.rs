aa27c212d3700ed22cbd5b5d0439ca6001782b97
Schneems <richard.schneeman+foo@gmail.com>
2024-01-24 15:40:27 -0600

    Removes the boxed trait state machine pattern and replaces it with struct returns.
    Boxed traits proved to be difficult to work with. On the user side: the buildpack user mostly interacts with associated methods on `BuildLog` they cannot be called unless the traits that implement them are in scope so the user was forced to have a `*` import. On the implementation side, boxed traits are limited. For example, there was no way to have an associated function that accepts a `Fn` or `FnOnce` to call it and return itself.
    
    The boxed trait state machine pattern was a useful exercise in refining the shape of the API, but it is no longer bringing joy. With that, we wish it farewell.

--- libherokubuildpack/src/output/mod.rs
+++ libherokubuildpack/src/output/mod.rs
@@ -1,6 +1,5 @@
 pub(crate) mod background;
 pub mod build_log;
-pub mod interface;
 pub mod section_log;
 pub mod style;
 mod util;
--- libherokubuildpack/src/output/section_log.rs
+++ libherokubuildpack/src/output/section_log.rs
@@ -1,45 +1,43 @@
+//! Write to the build output in a `Box<dyn SectionLogger>` format with functions
+//!
+//! ## What
+//!
+//! Logging from within a layer can be difficult because calls to the layer interface are not
+//! mutable nor consumable. Functions can be used at any time with no restrictions. The
+//! only downside is that the buildpack author (you) is now responsible for:
+//!
+//! - Ensuring that `Box<dyn StartedLogger>::section()` was called right before any of these
+//! functions are called.
+//! - Ensuring that you are not attempting to log while already logging i.e. calling `step()` within a
+//! `step_timed()` call.
+//!
+//! For usage details run `cargo run --bin print_style_guide`
+//!
+//! ## Use
+//!
+//! The main use case is logging inside of a layer:
+//!
+//! ```no_run
+//! use libherokubuildpack::output::section_log::log_step_timed;
+//!
+//! // fn create(
+//! //     &self,
+//! //     context: &libcnb::build::BuildContext<Self::Buildpack>,
+//! //     layer_path: &std::path::Path,
+//! // ) -> Result<
+//! //     libcnb::layer::LayerResult<Self::Metadata>,
+//! //     <Self::Buildpack as libcnb::Buildpack>::Error,
+//! // > {
+//! log_step_timed("Installing", || {
+//!         // Install logic here
+//!         todo!()
+//!     })
+//! // }
+//! ```
+use crate::output::build_log::StreamLog;
 use crate::output::build_log::{state, BuildData, BuildLog};
-#[allow(clippy::wildcard_imports)]
-pub use crate::output::interface::*;
 use std::io::Stdout;
 use std::marker::PhantomData;
-
-/// Write to the build output in a `Box<dyn SectionLogger>` format with functions
-///
-/// ## What
-///
-/// Logging from within a layer can be difficult because calls to the layer interface are not
-/// mutable nor consumable. Functions can be used at any time with no restrictions. The
-/// only downside is that the buildpack author (you) is now responsible for:
-///
-/// - Ensuring that `Box<dyn StartedLogger>::section()` was called right before any of these
-/// functions are called.
-/// - Ensuring that you are not attempting to log while already logging i.e. calling `step()` within a
-/// `step_timed()` call.
-///
-/// For usage details run `cargo run --bin print_style_guide`
-///
-/// ## Use
-///
-/// The main use case is logging inside of a layer:
-///
-/// ```no_run
-/// use libherokubuildpack::output::section_log::log_step_timed;
-///
-/// // fn create(
-/// //     &self,
-/// //     context: &libcnb::build::BuildContext<Self::Buildpack>,
-/// //     layer_path: &std::path::Path,
-/// // ) -> Result<
-/// //     libcnb::layer::LayerResult<Self::Metadata>,
-/// //     <Self::Buildpack as libcnb::Buildpack>::Error,
-/// // > {
-/// log_step_timed("Installing", || {
-///         // Install logic here
-///         todo!()
-///     })
-/// // }
-/// ```
 
 /// Output a message as a single step, ideally a short message
 ///
@@ -49,7 +47,7 @@ use std::marker::PhantomData;
 /// log_step("Clearing cache (ruby version changed)");
 /// ```
 pub fn log_step(s: impl AsRef<str>) {
-    logger().step(s.as_ref());
+    let _ = logger().step(s.as_ref());
 }
 
 /// Will print the input string followed by a background timer
@@ -67,7 +65,7 @@ pub fn log_step(s: impl AsRef<str>) {
 pub fn log_step_timed<T>(s: impl AsRef<str>, f: impl FnOnce() -> T) -> T {
     let timer = logger().step_timed(s.as_ref());
     let out = f();
-    timer.finish_timed_step();
+    let _ = timer.finish_timed_step();
     out
 }
 
@@ -88,13 +86,10 @@ pub fn log_step_timed<T>(s: impl AsRef<str>, f: impl FnOnce() -> T) -> T {
 /// ```
 ///
 /// Timing information will be output at the end of the step.
-pub fn log_step_stream<T>(
-    s: impl AsRef<str>,
-    f: impl FnOnce(&mut Box<dyn StreamLogger>) -> T,
-) -> T {
+pub fn log_step_stream<T>(s: impl AsRef<str>, f: impl FnOnce(&mut StreamLog<Stdout>) -> T) -> T {
     let mut stream = logger().step_timed_stream(s.as_ref());
     let out = f(&mut stream);
-    stream.finish_timed_stream();
+    let _ = stream.finish_timed_stream();
     out
 }
 
@@ -105,20 +100,20 @@ pub fn log_error(s: impl AsRef<str>) {
 
 /// Print an warning block to the output
 pub fn log_warning(s: impl AsRef<str>) {
-    logger().announce().warning(s.as_ref());
+    let _ = logger().announce().warning(s.as_ref());
 }
 
 /// Print an important block to the output
 pub fn log_important(s: impl AsRef<str>) {
-    logger().announce().important(s.as_ref());
+    let _ = logger().announce().important(s.as_ref());
 }
 
-fn logger() -> Box<dyn SectionLogger> {
-    Box::new(BuildLog::<state::InSection, Stdout> {
+fn logger() -> BuildLog<state::InSection, Stdout> {
+    BuildLog::<state::InSection, Stdout> {
         io: std::io::stdout(),
         // Be careful not to do anything that might access this state
         // as it's ephemeral data (i.e. not passed in from the start of the build)
         data: BuildData::default(),
         state: PhantomData,
-    })
+    }
 }
