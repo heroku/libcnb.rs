5d72f37c9ee0f8ac6cec0ca69e066369789ff681
Manuel Fuchs <manuel.fuchs@salesforce.com>
2024-03-12 17:11:07 +0100

    Update test buildpacks for new layer API

--- test-buildpacks/readonly-layer-files/src/main.rs
+++ test-buildpacks/readonly-layer-files/src/main.rs
@@ -1,12 +1,12 @@
-mod layer;
-
-use crate::layer::TestLayer;
 use libcnb::build::{BuildContext, BuildResult, BuildResultBuilder};
 use libcnb::data::layer_name;
 use libcnb::detect::{DetectContext, DetectResult, DetectResultBuilder};
 use libcnb::generic::{GenericMetadata, GenericPlatform};
+use libcnb::layer::{CachedLayerDefinition, InspectExistingAction, InvalidMetadataAction};
 use libcnb::{buildpack_main, Buildpack};
-use std::io::Error;
+use std::fs;
+use std::fs::Permissions;
+use std::os::unix::fs::PermissionsExt;
 
 // Suppress warnings due to the `unused_crate_dependencies` lint not handling integration tests well.
 #[cfg(test)]
@@ -24,7 +24,28 @@ impl Buildpack for TestBuildpack {
     }
 
     fn build(&self, context: BuildContext<Self>) -> libcnb::Result<BuildResult, Self::Error> {
-        context.handle_layer(layer_name!("test"), TestLayer)?;
+        let layer = context.cached_layer(
+            layer_name!("test"),
+            CachedLayerDefinition {
+                build: true,
+                launch: true,
+                invalid_metadata: &|_| InvalidMetadataAction::DeleteLayer,
+                inspect_existing: &|_: &GenericMetadata, _| InspectExistingAction::Delete,
+            },
+        )?;
+
+        let directory = layer.path().join("sub_directory");
+        fs::create_dir_all(&directory).map_err(TestBuildpackError::IOError)?;
+
+        fs::write(directory.join("foo.txt"), "hello world!")
+            .map_err(TestBuildpackError::IOError)?;
+
+        // By making the sub-directory read-only, files inside it cannot be deleted. This would
+        // cause issues when libcnb.rs tries to delete a cached layer directory unless libcnb.rs
+        // handles this case explicitly.
+        fs::set_permissions(&directory, Permissions::from_mode(0o555))
+            .map_err(TestBuildpackError::IOError)?;
+
         BuildResultBuilder::new().build()
     }
 }
@@ -34,9 +55,9 @@ pub(crate) enum TestBuildpackError {
     IOError(std::io::Error),
 }
 
-impl From<std::io::Error> for TestBuildpackError {
-    fn from(io_error: Error) -> Self {
-        Self::IOError(io_error)
+impl From<TestBuildpackError> for libcnb::Error<TestBuildpackError> {
+    fn from(error: TestBuildpackError) -> Self {
+        libcnb::Error::BuildpackError(error)
     }
 }
 
