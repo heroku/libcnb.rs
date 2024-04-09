239c01c69974b489726514413d4d1f010ce019df
Manuel Fuchs <manuel.fuchs@salesforce.com>
2024-04-09 10:53:44 +0200

    Properly expose layer errors

--- libcnb/src/layer/mod.rs
+++ libcnb/src/layer/mod.rs
@@ -4,6 +4,10 @@ pub(crate) mod shared;
 pub(crate) mod struct_api;
 pub(crate) mod trait_api;
 
+pub use shared::DeleteLayerError;
 pub use shared::LayerError;
+pub use shared::ReadLayerError;
+pub use shared::WriteLayerError;
+
 pub use struct_api::*;
 pub use trait_api::*;
