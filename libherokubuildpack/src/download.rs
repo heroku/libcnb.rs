d9d61d71f923071e07488556db88963ae89ad814
Colin Casey <casey.colin@gmail.com>
2025-09-17 12:26:49 -0300

    Revert changes and improve compatibility of deprecated functions

---
+++ libherokubuildpack/src/download.rs
@@ -0,0 +1,42 @@
+use crate::http::{HttpError, ResponseExt, get};
+use std::path::Path;
+
+#[deprecated(
+    note = "This has been replaced by `HttpError` and currently does nothing other than wrap `HttpError`"
+)]
+#[derive(thiserror::Error, Debug)]
+pub enum DownloadError {
+    #[error(transparent)]
+    HttpError(HttpError),
+}
+
+/// Downloads a file via HTTP(S) to a local path
+///
+/// # Examples
+/// ```
+/// use libherokubuildpack::digest::sha256;
+/// use libherokubuildpack::download::download_file;
+/// use tempfile::tempdir;
+///
+/// let temp_dir = tempdir().unwrap();
+/// let temp_file = temp_dir.path().join("result.bin");
+///
+/// download_file("https://example.com/", &temp_file).unwrap();
+/// assert_eq!(
+///     sha256(&temp_file).unwrap(),
+///     "ea8fac7c65fb589b0d53560f5251f74f9e9b243478dcb6b3ea79b5e36449c8d9"
+/// );
+/// ```
+#[deprecated(
+    note = "Use `get(uri).call_sync().and_then(|res| res.download_to_file_sync(destination)` instead"
+)]
+#[allow(deprecated)]
+pub fn download_file(
+    uri: impl AsRef<str>,
+    destination: impl AsRef<Path>,
+) -> Result<(), DownloadError> {
+    get(uri.as_ref())
+        .call_sync()
+        .and_then(|res| res.download_to_file_sync(destination))
+        .map_err(DownloadError::HttpError)
+}
