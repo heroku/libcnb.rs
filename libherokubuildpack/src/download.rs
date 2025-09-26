b095ab97520d7703c59e13ec5d74b8b8f3af45f3
Manuel Fuchs <manuel.fuchs@salesforce.com>
2025-09-26 11:45:44 +0200

    Replace ureq with reqwest

--- libherokubuildpack/src/download.rs
+++ libherokubuildpack/src/download.rs
@@ -1,15 +1,12 @@
 use std::{fs, io};
-use ureq::Agent;
-use ureq::tls::{RootCerts, TlsConfig};
 
 #[derive(thiserror::Error, Debug)]
 pub enum DownloadError {
-    // Boxed to prevent `large_enum_variant` errors since `ureq::Error` is massive.
     #[error("HTTP error while downloading file: {0}")]
-    HttpError(#[from] Box<ureq::Error>),
+    HttpError(#[from] reqwest::Error),
 
     #[error("I/O error while downloading file: {0}")]
-    IoError(#[from] std::io::Error),
+    IoError(#[from] io::Error),
 }
 
 /// Downloads a file via HTTP(S) to a local path.
@@ -37,19 +34,80 @@ pub fn download_file(
     uri: impl AsRef<str>,
     destination: impl AsRef<std::path::Path>,
 ) -> Result<(), DownloadError> {
-    let agent = Agent::config_builder()
-        .tls_config(
-            TlsConfig::builder()
-                .root_certs(RootCerts::PlatformVerifier)
-                .build(),
-        )
-        .build()
-        .new_agent();
+    let client = reqwest::blocking::ClientBuilder::new()
+        .use_rustls_tls()
+        .build()?;
 
-    let response = agent.get(uri.as_ref()).call().map_err(Box::new)?;
-    let mut reader = response.into_body().into_reader();
+    let mut response = client.get(uri.as_ref()).send()?;
     let mut file = fs::File::create(destination.as_ref())?;
-    io::copy(&mut reader, &mut file)?;
+
+    io::copy(&mut response, &mut file)?;
 
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::download_file;
+    use indoc::indoc;
+    use tempfile::NamedTempFile;
+
+    #[test]
+    fn test_self_signed_certificate() {
+        // Using unsafe to modify environment variables is not thread-safe but acceptable here
+        // since this test needs to simulate custom certificate scenarios and is the only test
+        // manipulating SSL_CERT_FILE. Since this test is testing that the function implicitly
+        // gets global state from environment variables, there is no other way of testing this.
+        unsafe {
+            std::env::remove_var("SSL_CERT_FILE");
+        }
+
+        let temp_file = NamedTempFile::new().unwrap();
+
+        assert!(download_file("https://self-signed.badssl.com", temp_file.path()).is_err());
+
+        let badssl_self_signed_cert_dir = tempfile::tempdir().unwrap();
+        let badssl_self_signed_cert = badssl_self_signed_cert_dir
+            .path()
+            .join("badssl_self_signed_cert.pem");
+
+        // https://github.com/rustls/rustls-native-certs/blob/main/tests/badssl-com-chain.pem
+        std::fs::write(
+            &badssl_self_signed_cert,
+            indoc! { "
+             -----BEGIN CERTIFICATE-----
+             MIIDeTCCAmGgAwIBAgIJAMnA8BB8xT6wMA0GCSqGSIb3DQEBCwUAMGIxCzAJBgNV
+             BAYTAlVTMRMwEQYDVQQIDApDYWxpZm9ybmlhMRYwFAYDVQQHDA1TYW4gRnJhbmNp
+             c2NvMQ8wDQYDVQQKDAZCYWRTU0wxFTATBgNVBAMMDCouYmFkc3NsLmNvbTAeFw0y
+             MTEwMTEyMDAzNTRaFw0yMzEwMTEyMDAzNTRaMGIxCzAJBgNVBAYTAlVTMRMwEQYD
+             VQQIDApDYWxpZm9ybmlhMRYwFAYDVQQHDA1TYW4gRnJhbmNpc2NvMQ8wDQYDVQQK
+             DAZCYWRTU0wxFTATBgNVBAMMDCouYmFkc3NsLmNvbTCCASIwDQYJKoZIhvcNAQEB
+             BQADggEPADCCAQoCggEBAMIE7PiM7gTCs9hQ1XBYzJMY61yoaEmwIrX5lZ6xKyx2
+             PmzAS2BMTOqytMAPgLaw+XLJhgL5XEFdEyt/ccRLvOmULlA3pmccYYz2QULFRtMW
+             hyefdOsKnRFSJiFzbIRMeVXk0WvoBj1IFVKtsyjbqv9u/2CVSndrOfEk0TG23U3A
+             xPxTuW1CrbV8/q71FdIzSOciccfCFHpsKOo3St/qbLVytH5aohbcabFXRNsKEqve
+             ww9HdFxBIuGa+RuT5q0iBikusbpJHAwnnqP7i/dAcgCskgjZjFeEU4EFy+b+a1SY
+             QCeFxxC7c3DvaRhBB0VVfPlkPz0sw6l865MaTIbRyoUCAwEAAaMyMDAwCQYDVR0T
+             BAIwADAjBgNVHREEHDAaggwqLmJhZHNzbC5jb22CCmJhZHNzbC5jb20wDQYJKoZI
+             hvcNAQELBQADggEBAC4DensZ5tCTeCNJbHABYPwwqLUFOMITKOOgF3t8EqOan0CH
+             ST1NNi4jPslWrVhQ4Y3UbAhRBdqXl5N/NFfMzDosPpOjFgtifh8Z2s3w8vdlEZzf
+             A4mYTC8APgdpWyNgMsp8cdXQF7QOfdnqOfdnY+pfc8a8joObR7HEaeVxhJs+XL4E
+             CLByw5FR+svkYgCbQGWIgrM1cRpmXemt6Gf/XgFNP2PdubxqDEcnWlTMk8FCBVb1
+             nVDSiPjYShwnWsOOshshCRCAiIBPCKPX0QwKDComQlRrgMIvddaSzFFTKPoNZjC+
+             CUspSNnL7V9IIHvqKlRSmu+zIpm2VJCp1xLulk8=
+             -----END CERTIFICATE-----
+         "},
+        )
+        .unwrap();
+
+        unsafe {
+            std::env::set_var("SSL_CERT_FILE", badssl_self_signed_cert);
+        }
+
+        assert!(download_file("https://self-signed.badssl.com", temp_file.path()).is_ok());
+
+        unsafe {
+            std::env::remove_var("SSL_CERT_FILE");
+        }
+    }
 }
